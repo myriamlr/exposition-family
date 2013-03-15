61fc1c790c2fd66ddefa4dfdd1de3d880147e6c4
derekbeaton <derekbeaton@303b699f-1cb0-0ac3-c600-ec55096e7429>
2013-03-15 03:58:27 +0000

    A change to epMDS to make it easier to understand and to create 2 needed functions (distances/weights; mdsTransform).

---
+++ ExPosition/R/makeDistancesAndWeights.R
@@ -0,0 +1,11 @@
+makeDistancesAndWeights <- function(DATA,method="euclidean"){
+	if(method=="chi2"){
+		chi2res <- chi2Dist(DATA)
+		D <- chi2res$D
+		MW <- list(M=chi2res$M)
+	}else{
+		D <- as.matrix(dist(DATA,method=method,diag=TRUE,upper=TRUE))
+		MW <- computeMW(D,masses=masses)		
+	}
+	return(list(D=D,MW=MW))
+}
