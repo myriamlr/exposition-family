f607b429ae138be94a5d4a97ed268a41c17b1b20
derekbeaton <derekbeaton@303b699f-1cb0-0ac3-c600-ec55096e7429>
2013-03-15 05:08:32 +0000

    Some updates to allow epGraphs, tepGraphs to use a color offset. colorSelection functions also allow an easier offset.

--- prettyGraphs/R/prettyGraphsHSVColorSelection.R
+++ prettyGraphs/R/prettyGraphsHSVColorSelection.R
@@ -1,10 +1,13 @@
 #wonderful stuff from here: http://martin.ankerl.com/2009/12/09/how-to-create-random-colors-programmatically/
 
 
-prettyGraphsHSVColorSelection <- function(n.colors=1,color.changer=abs((1-sqrt(5))/2),h=13,s=0.75,v=0.75){
+prettyGraphsHSVColorSelection <- function(n.colors=1,offset=NULL,h=13,s=0.75,v=0.75){
+	if(is.null(offset)){
+		offset <- abs((1-sqrt(5))/2)
+	}
 	these.cols <- c()
 	for(i in 1:n.colors){
-		h <- h + color.changer
+		h <- h + offset
 		h <- h %% 1
 		these.cols <- c(these.cols,hsv(h,s,v))
 	}
