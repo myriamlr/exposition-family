93168a74b8e8a050af4d2b9eaa99b9b215f37c94
derekbeaton@gmail.com <derekbeaton@gmail.com@303b699f-1cb0-0ac3-c600-ec55096e7429>
2013-09-13 04:28:08 +0000

    Installs Development versions of packages.

---
+++ Packages/Dev.Install.R
@@ -0,0 +1,13 @@
+#download.file("http://exposition-family.googlecode.com/svn/Packages/Development/prettyGraphs_2.1.4.tar.gz")
+#install.packages(file.path(tempdir(), 'DEV_prettyGraphs_2.1.4.tar.gz'),repos=NULL, type='source')
+
+file.names <- c('prettyGraphs_2.1.4.tar.gz','ExPosition_2.4.9.tar.gz','InPosition_0.10.5.tar.gz','TExPosition_2.6.7.tar.gz','TInPosition_0.10.5.tar.gz')
+web.loc <- c('http://exposition-family.googlecode.com/svn/Packages/Development/')
+temp.dir <- tempdir()
+for(i in 1:length(file.names)){
+	
+	download.file(paste(web.loc,file.names[i],sep=""),destfile=paste(temp.dir,file.names[i],sep=""))
+	install.packages(paste(temp.dir,file.names[i],sep=""),repos=NULL,type="source")
+	
+}
+
