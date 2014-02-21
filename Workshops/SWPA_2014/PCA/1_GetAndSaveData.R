c13f6c0c2c730f450c9d348e0e48263a31ff2fd5
derekbeaton@gmail.com <derekbeaton@gmail.com@303b699f-1cb0-0ac3-c600-ec55096e7429>
2014-02-21 20:45:43 +0000

    Updated the get/save file to read from the ExPo website.
    Also, we should keep in mind which directory/folder we are in (and would like to be in!).

--- Workshops/SWPA_2014/PCA/1_GetAndSaveData.R
+++ Workshops/SWPA_2014/PCA/1_GetAndSaveData.R
@@ -2,6 +2,11 @@
 ##This is good practice for analysis pipelines.
 
 
+##Which directory are we in?
+getwd()
+	##Do we want to change our directory? Usually, this is a good idea
+	# ?setwd() will help us!
+
 ##let's see if we have anything in the workspace:
 
 ls()
@@ -12,9 +17,11 @@ rm(list=ls()) #this gets rid of _everything_.
 
 
 	##let's discuss what each flag here means.
-ps.sim.data <- read.csv('ps.sim.data.csv',header=TRUE,row.names=1)
-ps.sim.design <- read.csv('ps.sim.design.csv',header=TRUE,row.names=1)
+#ps.sim.data <- read.csv('ps.sim.data.csv',header=TRUE,row.names=1)
+#ps.sim.design <- read.csv('ps.sim.design.csv',header=TRUE,row.names=1)
 
+ps.sim.data <- read.csv("http://exposition-family.googlecode.com/svn/Workshops/SWPA_2014/PCA/ps.sim.data.csv",header=TRUE,row.names=1)
+ps.sim.design <- read.csv("http://exposition-family.googlecode.com/svn/Workshops/SWPA_2014/PCA/ps.sim.data.csv",header=TRUE,row.names=1)
 
 ##it's often good practice to save the data in a natural R format
 ##Typically, after you've cleaned the data and it's ready to go!
